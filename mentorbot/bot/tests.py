bbbd20834f27974ec9a6bf7e1f4c28e3213237f7
Awinja <ingari61@gmail.com>
2018-01-20 22:43:57 +0300

    added some tests

--- mentorbot/bot/tests.py
+++ mentorbot/bot/tests.py
@@ -7,12 +7,28 @@ class ModelTestCase(TestCase):
     This class defines tests suites for the mentorbot models
     '''
     def setup(self):
-        pass
+        '''define test variables'''
+        self.mentordetails = {'Joan Awinja', '0725792909', 'joan.awinja@andela.com','python'}
+        self.mentorshipfieldname = {'java'}
+        self.menteerequestnewmentorshipfield = {'Mary Jane','python'}
+        self.newmentorshipfieldrequest = {'Jane Doe','approved','scala'}
+        self.newmentorshipfieldrequestunapproved = {'jerry kurata', 'unapproved','javascript'}
+        self.mentordetails = MentorDetails(self.mentordetails)
+        self.mentorshipfield = MentorshipFields(self.mentorshipfieldname)
+        self.menteerequestNewMentorship = MenteeRequestNewMentorshipField(self.menteerequestnewmentorshipfield)
+        self.newmentorshipfieldrequest = NewMentorshipFieldRequest(self.newmentorshipfieldrequest)
+        self.newmentorshipfieldrequestunapproved = NewMentorshipFieldRequest(self.newmentorshipfieldrequestunapproved)
+
+    
     '''
     Tests edge cases for adding, searching, editing, deleting a mentor 
     '''
     def test_adding_a_new_mentor(self):
-        pass
+        old_count = MentorDetails.objects.count()
+        self.mentordetails.save()
+        new_count = MentorDetails.objects.count()
+        self.assertNotEqual(old_count, new_count)
+
     def test_adding_a_mentor_missing_fields(self):
         pass
     def test_adding_an_existing_mentor(self):
@@ -38,15 +54,25 @@ class ModelTestCase(TestCase):
     Tests edge cases for a mentor requesting to mentor a field that does not exist in the Database
     '''
     def test_adding_a_new_mentorship_field(self):
-        pass
+        old_count = NewMentorshipFieldRequest.objects.count()
+        self.newmentorshipfieldrequest.save()
+        new_count = NewMentorshipFieldRequest.objects.count()
+        self.assertNotEqual(old_count, new_count)
+
     def test_unapproved_mentor_requesting(self):
-        pass
+        old_count = NewMentorshipFieldRequest.objects.count()
+        self.newmentorshipfieldrequestunapproved.save()
+        new_count = NewMentorshipFieldRequest.objects.count()
+        self.assertEqual(old_count, new_count)
     
     '''
     Tests edge cases for a mentee requesting for a new mentorship field
     '''
     def tests_requesting_new_mentorshipfield(self):
-        pass
+        old_count = MenteeRequestNewMentorshipField.objects.count()
+        self.menteerequestNewMentorship.save()
+        new_count = MenteeRequestNewMentorshipField.objects.count()
+        self.assertNotEqual(old_count, new_count)
 
     
     

d1436e431ee7d2cb7700bb68b36add4688380145
Awinja <ingari61@gmail.com>
2018-01-25 11:39:35 +0300

    changed file structure

--- MenteeRequests/views.py
+++ MenteeRequests/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from ..serializers import MenteeRequestsSerializer
+from mentorbot.serializers  import MenteeRequestsSerializer
 from .models import  MenteeRequests
 
 class CreateView(generics.ListCreateAPIView):

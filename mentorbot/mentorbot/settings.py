d15f890492182628277bd3216a1a06039cfc5f1d
Joan Awinja Ingari <joan.awinja@andela.com>
2018-06-13 15:29:37 +0300

    changed settings

--- mentorbot/mentorbot/settings.py
+++ mentorbot/mentorbot/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_ROOT = '/bot/static'
-STATIC_URL = 'bot/static'
+STATIC_URL = 'bot/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'bot/static'),
 )
 

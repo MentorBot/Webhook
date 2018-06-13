6d808a90e07c6d487dabf7c81135edb21028d1ba
Joan Awinja Ingari <joan.awinja@andela.com>
2018-06-13 15:37:24 +0300

    moved static

--- mentorbot/mentorbot/settings.py
+++ mentorbot/mentorbot/settings.py
@@ -123,9 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = '/bot/static'
-STATIC_URL = 'bot/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'bot/static'),
+STATIC_ROOT = 'static'
+STATIC_URL = 'static'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),
 )
 
 

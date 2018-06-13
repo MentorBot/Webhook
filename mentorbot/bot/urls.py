5cca0ed38ae3eb863f498b78dbfd16a4bc7f74e3
Joan Awinja Ingari <joan.awinja@andela.com>
2018-06-13 14:39:09 +0300

    changed URL

--- mentorbot/bot/urls.py
+++ mentorbot/bot/urls.py
@@ -6,7 +6,7 @@ from .admin import admin
 
 urlpatterns = {
     url('^$', TemplateView.as_view(template_name='index.html')),
-    url(r'^messenger_webhook', FacebookMessengerWebhook.as_view(), name="Facebook Messenger Webhook"),
+    url(r'^messenger_webhook/?$', FacebookMessengerWebhook.as_view(), name="Facebook Messenger Webhook"),
     url(r'^slack_webhook$', SlackWebhook.as_view(), name="Slack Webhook"),
     url(r'^twitter_webhook$', TwitterWebhook.as_view(), name="Twitter Webhook"),
     url(r'^chatbot_responses$', ChatBotResponse.as_view(), name="ChatBot Response"),

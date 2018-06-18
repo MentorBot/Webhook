eda05823ede9036f08f0777748fa5e06680e7758
Joan Awinja Ingari <joan.awinja@andela.com>
2018-06-18 14:48:04 +0300

    delete collectstatic.py

--- collectstatic.py
+++
@@ -1,9 +0,0 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
-
-sched = BlockingScheduler()
-
-@sched.scheduled_job('interval', minutes=3)
-def times_job():
-    return 'heroku run cd mentorbot && python manage.py collectstatic'
-
-sched.start()

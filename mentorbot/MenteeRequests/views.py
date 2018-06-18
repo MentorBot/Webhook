dfadb296412df0ab3b3e2d9615348b937865c155
Joan Awinja Ingari <joan.awinja@andela.com>
2018-06-18 16:45:47 +0300

    add get to menteerequests

--- mentorbot/MenteeRequests/views.py
+++ mentorbot/MenteeRequests/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from django.http.response import HttpResponse
 from mentorbot.serializers  import MenteeRequestsSerializer
 from .models import  MenteeRequests
 
@@ -6,13 +7,16 @@ class CreateView(generics.ListCreateAPIView):
     queryset = MenteeRequests.objects.all()
     serializer_class = MenteeRequestsSerializer
 
-    def perform_create(self, serializer):
+    def create(self, serializer):
         """Save the post data when creating a new Mentee Request."""
         serializer.save()
 
 class ListView(generics.ListAPIView):
     queryset = MenteeRequests.objects.all()
     serializer_class = MenteeRequestsSerializer
+
+    def get(self, requests):
+        return HttpResponse('get')
 
 class RetrieveView(generics.RetrieveAPIView):
     queryset = MenteeRequests.objects.all()

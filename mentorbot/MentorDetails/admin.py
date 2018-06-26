e37b2f6fa0a546e0df2414575b7572c8d7a18243
Joan Awinja Ingari <joan.awinja@andela.com>
2018-06-26 17:25:51 +0300

    changed admin

--- mentorbot/MentorDetails/admin.py
+++ mentorbot/MentorDetails/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import MentorDetails
+from .models import MentorUser, MentorProfile
 
-admin.site.register(MentorDetails)
+admin.site.register(MentorUser)
+admin.site.register(MentorProfile)

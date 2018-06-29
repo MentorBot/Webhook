a39373bf014200b76ec0ce64e9333570cb0a877c
Joan Awinja Ingari <joan.awinja@andela.com>
2018-06-29 12:10:04 +0300

    removed url

--- mentorbot/bot/urls.py
+++ mentorbot/bot/urls.py
@@ -1,7 +1,7 @@
 
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import FacebookMessengerWebhook, SlackWebhook, TwitterWebhook, ChatBotResponse
+from .views import FacebookMessengerWebhook, SlackWebhook, TwitterWebhook
 from . import main
 
 
@@ -12,7 +12,6 @@ urlpatterns = {
     url(r'^messenger_webhook/?$', FacebookMessengerWebhook.as_view(), name="Facebook Messenger Webhook"),
     url(r'^slack_webhook$', SlackWebhook.as_view(), name="Slack Webhook"),
     url(r'^twitter_webhook$', TwitterWebhook.as_view(), name="Twitter Webhook"),
-    url(r'^chatbot_responses$', ChatBotResponse.as_view(), name="ChatBot Response"),
 
 }
 urlpatterns = format_suffix_patterns(urlpatterns)

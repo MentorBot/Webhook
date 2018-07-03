5ebde44b0738a41313257f7e3542a3add1163e08
Joan Awinja Ingari <joan.awinja@andela.com>
2018-07-03 15:03:04 +0300

    add html

--- mentorbot/bot/main.py
+++ mentorbot/bot/main.py
@@ -11,6 +11,11 @@ def index(request):
 def become_mentor(request):
     return render(request, '../templates/generic.html')
 
-
 def find_mentor(request):
     return render(request, '../templates/elements.html')
+
+def mentorregister(request):
+    return render(request, '../templates/mentorregister.html')
+
+def mentorsearch(request):
+    return render(request, '../templates/mentorsearch.html')
--- mentorbot/bot/urls.py
+++ mentorbot/bot/urls.py
@@ -8,8 +8,8 @@ from . import main
 urlpatterns = {
     url(r'^$', main.index, name='index'),
     url(r'^become_mentor$', main.become_mentor, name='become_mentor'),
-    url(r'^bot/register$', main.become_mentor, name='become_mentor'),
-    url(r'^bot/search$', main.become_mentor, name='become_mentor'),
+    url(r'^bot/register$', main.mentorregister, name='become_a_mentor'),
+    url(r'^bot/search$', main.mentorsearch, name='find_a_mentor'),
     url(r'^find_mentor$', main.find_mentor, name='find_mentor'),
     url(r'^messenger_webhook/?$', FacebookMessengerWebhook.as_view(), name="Facebook Messenger Webhook"),
     url(r'^slack_webhook$', SlackWebhook.as_view(), name="Slack Webhook"),

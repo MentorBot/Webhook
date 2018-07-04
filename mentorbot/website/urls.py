f7da08a2a33fe0bea5d8d7dc10a9b25b89daee75
Joan Awinja Ingari <joan.awinja@andela.com>
2018-07-04 22:20:48 +0300

    new app

--- mentorbot/website/urls.py
+++ mentorbot/website/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = {
     url(r'^become_mentor$', views.become_mentor, name='become_mentor'),
     url(r'^find_mentor$', views.find_mentor, name='find_mentor'),
-    url(r'^index$', views.index, name='index'),
+    url(r'^/$', views.index, name='index'),
 }
 urlpatterns = format_suffix_patterns(urlpatterns)

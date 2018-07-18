6baf327b87147f18e7b2c424e5fefa097d090b5c
Joan Awinja Ingari <joan.awinja@andela.com>
2018-07-18 17:31:19 +0300

    working on cors

--- mentorbot/mentorbot/settings/base.py
+++ mentorbot/mentorbot/settings/base.py
@@ -86,6 +86,25 @@ MIDDLEWARE = [
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+)
 
 ROOT_URLCONF = 'mentorbot.urls'
 

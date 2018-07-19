b23894b1030a4333a9d9b611275f4391ed5e50b1
Joan Awinja Ingari <joan.awinja@andela.com>
2018-07-19 19:37:25 +0300

    db changes

--- mentorbot/MentorshipFields/models.py
+++ mentorbot/MentorshipFields/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from MentorDetails.models import MentorProfile
+# from MentorDetails.models import MentorProfile
 
 class MentorshipFields(models.Model):
     field_name = models.CharField(max_length=20, unique=True)
     field_details = models.CharField(max_length=20, unique=True)
-    field_mentors = models.ManyToManyField('MentorProfile')
+    # field_mentors = models.ManyToManyField('MentorProfile')
 
     class Meta:
         ordering = ('field_name',)

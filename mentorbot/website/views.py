aa0495c0abcc384a81b06bf885047896fecd1c1d
Joan Awinja Ingari <joan.awinja@andela.com>
2018-07-21 15:00:00 +0300

    url

--- mentorbot/website/views.py
+++ mentorbot/website/views.py
@@ -105,7 +105,9 @@ def find_mentor(request):
         data = json.dumps(data)
         print('-----data', data)
         response = requests.get(api_url + 'mentorshipfield_search/', params=data, headers=headers)
+        count = json.dumps(response)
         print('-----response1', response)
+        print('-------count', count)
         if response.data.count == 0:
             return render(request, '../templates/find_mentor.html', {'error': 'error'})
         else:

c57a5e356f2c7a3e0429505d4984af43eeffec5f
Joan Awinja Ingari <joan.awinja@andela.com>
2018-07-22 23:30:57 +0300

    error

--- mentorbot/website/views.py
+++ mentorbot/website/views.py
@@ -189,13 +189,15 @@ def mentor_login(request):
         data = json.dumps(user)
         response = requests.get(api_url + 'auth/login', data=data, headers=headers)
         if response.status_code is 200:
+            print('-----chilli', response)
+            get_mentor = response.content
             return render(request, '../templates/profile.html', {"get_mentor": get_mentor})
         else:
             r = response.content
             print('----login', r)
             error = r.message
             return render(request, '../templates/login.html'), {'error': error}
-            
+
 def reset_password(request):
     if request.method == 'GET':
         return render(request, '../templates/login.html')

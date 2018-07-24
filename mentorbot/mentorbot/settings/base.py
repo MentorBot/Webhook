bf6ed27567df589fd5fe46a7b4fe56d7a4ff82aa
Joan Awinja Ingari <joan.awinja@andela.com>
2018-07-24 18:32:18 +0300

    mail

--- mentorbot/mentorbot/settings/base.py
+++ mentorbot/mentorbot/settings/base.py
@@ -57,7 +57,7 @@ INSTALLED_APPS = [
 
 # AUTH_USER_MODEL ='MentorDetails.MentorUser'
 # AUTH_PROFILE_MODULE ='MentorDetails.MentorProfile'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')

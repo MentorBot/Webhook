b9670c1859ec05e328513a1a23ee9715353b9e1f
Joan Awinja Ingari <joan.awinja@andela.com>
2018-07-24 16:25:10 +0300

    life

--- mentorbot/mentorbot/settings/production.py
+++ mentorbot/mentorbot/settings/production.py
@@ -4,16 +4,16 @@ DEBUG = True
 
 SECRET_KEY = config('SECRET_KEY')
 
-# DATABASES = {
-#     'default': dj_database_url.config()
-# }
-
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'mentor-bots',
-       'USER': 'jus_machungwa',
-       'PASSWORD': 'Okusimba@1',
-       'HOST': 'localhost',
-       'PORT': '',
-}}
+    'default': dj_database_url.config()
+}
+
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'mentor-bots',
+#        'USER': 'jus_machungwa',
+#        'PASSWORD': 'Okusimba@1',
+#        'HOST': 'localhost',
+#        'PORT': '',
+# }}

c01f4521f49dca1adb23a429244527109ca72934
Joan Awinja Ingari <joan.awinja@andela.com>
2019-02-20 13:48:28 +0300

    db

--- mentorbot/mentorbot/settings/production.py
+++ mentorbot/mentorbot/settings/production.py
@@ -2,7 +2,18 @@ from mentorbot.settings.base import *
 
 DEBUG = True
 
+# SECRET_KEY = config('SECRET_KEY')
 
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': 'postgres://jktfggrohrfhyd:dfb17db90e4dc829dd04582d10f1664d8c1b9eca4e5340c2ab9ff92d22b3af12@ec2-50-19-86-139.compute-1.amazonaws.com:5432/d4fmv6idbe7bn3'
 }
+
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'mentor-bots',
+#        'USER': 'jus_machungwa',
+#        'PASSWORD': 'Okusimba@1',
+#        'HOST': 'localhost',
+#        'PORT': '',
+# }}
